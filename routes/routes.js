59527f4e349cfedec1dbb622d401ac5e6c0ecbbe
ichuvashov <ichuvashov@htc-cs.ru>
2020-08-19 13:15:16 +0400

    first init

---
+++ routes/routes.js
@@ -0,0 +1,20 @@
+const users = [{
+    id: 1,
+    name: "Richard Hendricks",
+    email: "richard@piedpiper.com",
+},
+    {
+        id: 2,
+        name: "Bertram Gilfoyle",
+        email: "gilfoyle@piedpiper.com",
+    },
+];
+
+const router = app => {
+    app.get('/', (request, response) => response.sendFile("C:/Users/ichuvashov/Desktop/calculator/index.html"));
+    app.get('/users', (request, response) => {
+        response.send(users);
+    });
+};
+
+module.exports = router;
