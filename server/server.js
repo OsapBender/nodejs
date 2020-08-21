e1e3fc4773f497461ab7fcefe268b9681751992e
ichuvashov <ichuvashov@htc-cs.ru>
2020-08-21 17:40:21 +0400

    сделал форму авторизации

--- server/server.js
+++ server/server.js
@@ -1,12 +1,49 @@
 const express = require("express");
+const cors = require('cors');
 const path = require("path");
 const http = require("http");
+const bodyParser = require("body-parser");
 
-const publicPath = path.join(__dirname, "./public");
+const publicPath = path.join(__dirname, "../build");
 const port = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
 
+const users = [{username: 'admin', password: '123'}];
+const corsOptions = {
+    origin: 'http://localhost:8080'
+};
+/*
+// allow cross origin requests for api
+app.post('/*', function (req, res, next) {
+    res.set({
+        'Access-Control-Allow-Origin': 'http://localhost:8080',
+        'Access-Control-Allow-Methods': 'POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    });
+    next();
+}); */
+
+// app.use(cors(corsOptions));
+app.use(bodyParser.json());
 app.use(express.static(publicPath));
+app.use('/auth', express.static(publicPath));
+
+// eslint-disable-next-line consistent-return
+app.post('/signin', (req, res) => {
+    if (!req.body) return res.sendStatus(400);
+    const {username} = req.body;
+    const {password} = req.body;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const user of users) {
+        if (user.username === username && user.password === password) {
+            return res.send(true);
+        }
+    }
+    res.send(false);
+});
+
+app.all("/*", (req, res) => res.sendStatus(404));
+
 server.listen(port, () => console.log("Сервер запущен на порте " + port));
--- src/components/auth/auth.jsx
+++ src/components/auth/auth.jsx
@@ -1,22 +1,86 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
-import { TextField } from "@material-ui/core";
+import {TextField, Button} from "@material-ui/core";
+import axios from "axios";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
   width: 400px;
-  height: 500px;
+  height: 300px;
   padding: 20px;
   border-radius: 5px;
-  box-shadow: 0 0 10px 10px rgba(0, 0, 0, 0.3);
+  box-shadow: 0 0 20px 5px rgba(0, 0, 0, 0.3);
 `;
 
+const Fact = styled.div`
+  display:flex;
+  flex-direction:column;
+  margin-top: 50px;
+  
+  div {
+  margin-bottom: 10px;
+  }
+`;
 
-export default () => (
-    <Form>
-        
-        <TextField id='outlined-basic' label='Username' variant='outlined' />
-        <TextField id='outlined-basic' label='Password' variant='outlined' />
-    </Form>
-);
+const WrapperButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+`;
+
+// eslint-disable-next-line consistent-return
+const submitForm = async (username, password, setError, props) => {
+    let resp = await axios.post('http://localhost:3000/signin', {username, password});
+    if (!resp.data) return setError(true);
+    setError(false);
+    props.history.push('/chat');
+};
+
+export default (props) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, serError] = useState(false);
+    return (
+        <Container>
+            <Form>
+                <Fact>
+                    <TextField
+                        id={ error ? 'outlined-error' : 'outlined-basic' }
+                        label={ error ? 'Wrong username' : 'Username' }
+                        variant='outlined'
+                        error={ !!error }
+                        onChange={ (e) => setUsername(e.target.value) }
+                    />
+                    <TextField
+                        id={ error ? 'outlined-error' : 'outlined-password-input' }
+                        label={ error ? 'Wrong password' : 'Password' }
+                        type='password'
+                        autoComplete='current-password'
+                        variant='outlined'
+                        error={ !!error }
+                        onChange={ (e) => setPassword(e.target.value) }
+                    />
+                </Fact>
+                <WrapperButton>
+                    <Button
+                        variant='contained'
+                        size='large'
+                        color='primary'
+                        onClick={ () => submitForm(username, password, serError, props) }
+                    >
+                        Войти
+                    </Button>
+                </WrapperButton>
+            </Form>
+        </Container>
+    )
+};
