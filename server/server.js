807427a620440487903240cdc474c662c602d7ad
ichuvashov <ichuvashov@htc-cs.ru>
2020-08-22 19:10:21 +0400

    добавил mongodb

--- server/server.js
+++ server/server.js
@@ -1,39 +1,58 @@
 const express = require("express");
-const cors = require('cors');
+const MongoClient = require("mongodb").MongoClient;
+const cors = require("cors");
 const path = require("path");
-const http = require("http");
+// const http = require("http");
 const bodyParser = require("body-parser");
 
 const publicPath = path.join(__dirname, "../build");
 const port = process.env.PORT || 3000;
-
+const mongoClient = new MongoClient(
+  "mongodb+srv://ichuvashov:9)8Af4jfwc@cluster0.dvlqq.mongodb.net/reactChat?retryWrites=true&w=majority"
+);
 const app = express();
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-const users = [{username: 'admin', password: '123'}];
 const corsOptions = {
-    origin: 'http://localhost:8080'
+  origin: "http://localhost:8080",
 };
+let dbClient;
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(express.static(publicPath));
-app.use('/auth', express.static(publicPath));
+app.use("/auth", express.static(publicPath));
+
+mongoClient.connect(function(err, client) {
+  if (err) return console.log(err);
+  dbClient = client;
+  app.locals.collection = client.db("reactChat").collection("users");
+  app.listen(port, function() {
+    console.log("Сервер запущен на порте " + port);
+  });
+});
 
 // eslint-disable-next-line consistent-return
-app.post('/signin', (req, res) => {
-    if (!req.body) return res.sendStatus(400);
-    const {username} = req.body;
-    const {password} = req.body;
-    // eslint-disable-next-line no-restricted-syntax
-    for (const user of users) {
-        if (user.username === username && user.password === password) {
-            return res.send(true);
-        }
+app.post("/signin", (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+  const { username } = req.body;
+  const { password } = req.body;
+  const { collection } = req.app.locals;
+  const obj = {};
+  obj[username] = password;
+  collection.findOne(obj, function(err, users) {
+    if (err) return console.log(err);
+    if (users) {
+      return res.send(true);
     }
-    res.send(false);
+  });
+
+  res.send(false);
 });
 
 app.all("/*", (req, res) => res.sendStatus(404));
 
-server.listen(port, () => console.log("Сервер запущен на порте " + port));
+process.on("SIGINT", () => {
+  dbClient.close();
+  process.exit();
+});
